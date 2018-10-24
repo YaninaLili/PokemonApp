fcf4ce76fc9daf6d55124596fad8e5e9e9b048af
Yanina Lili <lilisapallanay@gmail.com>
2018-10-24 12:00:59 -0500

    Agregada clase pokebola

---
+++ PokemonApp/src/pokemonapp/PokeBola.java
@@ -0,0 +1,15 @@
+
+package pokemonapp;
+
+public class PokeBola {
+    
+    private String tipos;
+
+    public String getTipos() {
+        return tipos;
+    }
+
+    public void setTipos(String tipos) {
+        this.tipos = tipos;
+    }
+}
